db8deeff40f3edd9da00316f7103f809d5049fc7
Ianis Vasilev <ianis@ivasilev.net>
2021-05-21 02:49:26 +0300

    Replace the existing lua code with expl3 code

--- lua/citation_target.lua
+++
@@ -1,7 +0,0 @@
-function citation_target(target)
-  if tonumber(target, 10) ~= nil then
-    tex.print("\\\\{}page " .. target)
-  elseif target ~= nil then
-    tex.print("\\\\" .. target)
-  end
-end
