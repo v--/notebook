4a2a82d40cc8846ebc5258420f11e4f40309e3dd
Ianis Vasilev <ianis@ivasilev.net>
2021-12-07 05:25:13 +0200

    Revamp the build system and add a script for formatting matrices

--- makefile
+++ makefile
@@ -1,37 +1,33 @@
 COMPILER := pdflatex -interaction=batchmode
-FIGURES_TEX_PDF := $(patsubst figures/%.tex,figures/%.pdf,$(wildcard figures/*.tex))
-FIGURES_ASY_PDF := $(patsubst figures/%.asy,figures/%.pdf,$(wildcard figures/*.asy))
+FIGURES_TEX_PDF := $(patsubst figures/%.tex,output/%.pdf,$(wildcard figures/*.tex))
+FIGURES_ASY_PDF := $(patsubst figures/%.asy,output/%.pdf,$(wildcard figures/*.asy))
 DOC_SOURCE := notebook.cls notebook.tex bib/*.bib packages/*.sty src/*.tex $(FIGURES_TEX_PDF) $(FIGURES_ASY_PDF)
 
-.PHONY: full figures watch clean
+.PHONY: full format-figures figures watch clean
 
-full: $(DOC_SOURCE)
+notebook.pdf: $(DOC_SOURCE)
 	$(COMPILER) -draftmode notebook.tex
 	biber notebook.bcf
 	$(COMPILER) -draftmode notebook.tex
-	$(COMPILER) notebook.tex
+	$(COMPILER) -output-directory=aux notebook.tex
+	cat aux/notebook.pdf > output/notebook.pdf
 
-notebook.pdf: $(DOC_SOURCE)
-	$(COMPILER) notebook.tex
+output/%.pdf: tikzcd.cls packages/*.sty figures/%.tex
+	$(COMPILER) -output-directory=aux figures/$*.tex
+	cat aux/$*.pdf > output/$*.pdf
 
-figures/%.pdf: tikzcd.cls packages/*.sty figures/%.tex
-	cd figures && $(COMPILER) $*.tex
-
-figures/%.pdf: figures/%.asy
-	cd figures && asy $*.asy
+output/%.pdf: figures/%.asy
+	asy -outname=aux/$*.asy figures/$*.asy
+	cat aux/$*.pdf > output/$*.pdf
 
 figures: $(FIGURES_TEX_PDF) $(FIGURES_ASY_PDF)
+
+format-figures:
+	@poetry run python format-tex-matrices.py figures/*.tex
 
 # I have implemented a very useful build system for LaTeX with log processing and debouncing
 watch:
 	@poetry run python watcher.py
-
-clean:
-	rm -fv {,figures/}*.pdf
-	rm -fv {,src/,figures/}*.log # tex
-	rm -fv {,src/,figures/}*.{aux,out,fls,toc} # latex
-	rm -fv {,src/}*.{bbl,bcf,blg,run.xml} # biber
-	rm -fv git-commit-info
 
 git-commit-info: .git/refs/heads/master
 	LC_ALL=en_BG.UTF-8 git log --max-count 1 --format=format:'hash={%h},date={%cd}' --date='format:%d %B %Y' HEAD > git-commit-info
--- watcher.py
+++ watcher.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from datetime import datetime, timedelta
 from fnmatch import fnmatch
 from timeit import default_timer as timer
@@ -5,23 +7,25 @@ from typing import Optional
 import asyncio
 import os.path
 import pathlib
+import shutil
 
 from asyncinotify import Inotify, Mask
 from loguru import logger
+from loguru._logger import Logger
 import texoutparse
 
 
 DEBOUNCE_THRESHOLD = timedelta(seconds=1)
 TEX_LOG_ENCODING = 'latin-1'
+ROOT_DIR = pathlib.Path('.')
+AUX_DIR = ROOT_DIR / 'aux'
+OUTPUT_DIR = ROOT_DIR / 'output'
 
 
 class Task:
-    modified_file: str
     command: str
+    sublogger: Logger
     out_buffer: int
-
-    def __init__(self):
-        self.sublogger = logger.bind(name=self.command)
 
     def __eq__(self, other):
         return isinstance(other, Task) and self.command == other.command
@@ -29,78 +33,82 @@ class Task:
     def __hash__(self):
         return hash(self.command)
 
-    async def pre_process(self):
-        return []
+    async def pre_process(self, runner: TaskRunner):
+        pass
 
-    async def post_process(self):
-        return []
+    async def post_process(self, runner: TaskRunner):
+        pass
+
+    async def on_failure(self, runner: TaskRunner):
+        pass
 
 
 class BiberTask(Task):
-    modified_file: str
     out_buffer: int = asyncio.subprocess.PIPE
+    biber_path: pathlib.Path
+    tex_path: pathlib.Path
 
-    def __init__(self, modified_file: str):
-        self.modified_file = modified_file
-        super().__init__()
-
-    def __repr__(self):
-        return f'BiberTask({repr(self.modified_file)})'
-
-    @property
-    def command(self):
-        return f'biber {self.modified_file}'
-
-
-class TeXTask(Task):
-    tex_file: str
-    out_buffer: int = asyncio.subprocess.DEVNULL
-    _bcf_file_hash: Optional[int] = None
-
-    def __init__(self, modified_file: str, tex_file: str):
-        self.modified_file = modified_file
-        self.tex_file = tex_file
-        super().__init__()
+    def __init__(self, biber_path: pathlib.Path, tex_path: pathlib.Path):
+        self.biber_path = biber_path
+        self.tex_path = tex_path
+        self.sublogger = logger.bind(name=str(self.biber_path))
 
     def __repr__(self):
-        return f'TeXTask({repr(self.base_name)})'
+        return f'BiberTask({repr(self.biber_path)})'
 
     @property
     def base_name(self):
-        return os.path.splitext(self.tex_file)[0]
-
-    @property
-    def pdf_file(self):
-        return self.base_name + '.pdf'
-
-    @property
-    def log_file(self):
-        return self.base_name + '.log'
-
-    @property
-    def bcf_file(self):
-        return self.base_name + '.bcf'
+        return os.path.splitext(self.biber_path)[0]
 
     @property
     def command(self):
-        return f'make {self.pdf_file}'
+        return f'biber {self.biber_path}'
+
+    async def post_process(self, runner: TaskRunner):
+        runner.schedule(TeXTask(self.tex_path), str(self.biber_path))
+
+
+class TeXTask(Task):
+    tex_path: pathlib.Path
+    out_buffer: int = asyncio.subprocess.DEVNULL
+    _bcf_file_hash: Optional[int] = None
+
+    def __init__(self, tex_path: pathlib.Path):
+        self.tex_path = tex_path
+        self.sublogger = logger.bind(name=str(self.tex_path))
+
+    def __repr__(self):
+        return f'TeXTask({repr(self.tex_path)})'
+
+    def get_aux_path(self, extension: str):
+        return AUX_DIR / self.tex_path.with_suffix(extension).name
+
+    @property
+    def build_pdf_path(self):
+        return OUTPUT_DIR / self.tex_path.with_suffix('.pdf').name
+
+    @property
+    def command(self):
+        return f'pdflatex -interaction=batchmode -output-directory={AUX_DIR} {self.tex_path}'
 
     def get_bcf_hash(self) -> Optional[int]:
         try:
-            with open(self.bcf_file, 'r') as bcf_file:
+            with open(self.get_aux_path('.bcf'), 'r') as bcf_file:
                 return hash(bcf_file.read())
         except IOError:
             return None
 
-    async def pre_process(self):
+    async def pre_process(self, runner: TaskRunner):
         self._bcf_file_hash = self.get_bcf_hash()
-        return []
 
-    async def post_process(self):
+    async def post_process(self, runner: TaskRunner):
         parser = texoutparse.LatexLogParser()
+        requires_rerun = False
 
         try:
-            with open(self.log_file, 'r', encoding=TEX_LOG_ENCODING) as log_file:
+            with open(self.get_aux_path('.log'), 'r', encoding=TEX_LOG_ENCODING) as log_file:
+                requires_rerun = 'Rerun to get' in log_file.read()
+                log_file.seek(0)
                 parser.process(log_file)
         except OSError:
             self.sublogger.error('Could not open TeX log file.')
@@ -111,44 +119,62 @@ class TeXTask(Task):
                 self.sublogger.warning(f'Compiled with {len(parser.warnings)} warnings. The first warning is:\n {str(parser.warnings[0])}')
             elif len(parser.badboxes) > 0:
                 self.sublogger.warning(f'Compiled with {len(parser.badboxes)} bad boxes. The first bad box is:\n {str(parser.badboxes[0])}')
-            else:
-                self.sublogger.info('Compiled without warnings.')
 
-        if self.get_bcf_hash() != self._bcf_file_hash:
-            return [BiberTask(self.bcf_file)]
+        if len(parser.errors) != 0:
+            return
 
-        return []
+        if self._bcf_file_hash != None and self.get_bcf_hash() != self._bcf_file_hash:
+            runner.schedule(BiberTask(self.get_aux_path('.bcf'), self.tex_path), str(self.tex_path))
+
+        if requires_rerun:
+            runner.schedule(self, 'last build')
+        else:
+            self.sublogger.debug(f'No more passes required. Copying {self.get_aux_path(".pdf")} to {self.build_pdf_path}')
+            shutil.copyfile(self.get_aux_path('.pdf'), self.build_pdf_path)
+
+    on_failure = post_process
 
 
-class MakeTask(Task):
-    make_task: str
+class AsymptoteTask(Task):
+    src_path: pathlib.Path
     out_buffer: int = asyncio.subprocess.PIPE
 
-    def __init__(self, modified_file: str, make_task: str):
-        self.modified_file = modified_file
-        self.make_task = make_task
-        super().__init__()
+    def __init__(self, src_path: pathlib.Path):
+        self.src_path = src_path
+        self.sublogger = logger.bind(name=str(self.src_path))
 
     def __repr__(self):
-        return f'MakeTask({repr(self.make_task)})'
+        return f'AsymptoteTask({repr(self.src_path)})'
+
+    @property
+    def aux_pdf_path(self):
+        return AUX_DIR / self.src_path.with_suffix('.pdf').name
+
+    @property
+    def build_pdf_path(self):
+        return OUTPUT_DIR / self.src_path.with_suffix('.pdf').name
 
     @property
     def command(self):
-        return f'make {self.make_task}'
+        return f'asy -outname={self.aux_pdf_path} {self.src_path}'
+
+    async def post_process(self, runner: TaskRunner):
+        shutil.copyfile(self.aux_pdf_path, self.build_pdf_path)
 
 
 class TaskRunner:
     active_tasks: set[Task] = set()
     last_run_attempt: dict[Task, datetime] = {}
 
-    async def run_task(self, task: Task):
-        task.sublogger.info(f'Running because {task.modified_file} was modified.')
+    async def run_task(self, task: Task, trigger: Optional[str] = None):
         self.active_tasks.add(task)
-
+        await task.pre_process(self)
         start = timer()
 
-        for new_task in await task.pre_process():
-            await self.run_task(new_task)
+        if trigger is None:
+            task.sublogger.info(f'Manually triggered')
+        else:
+            task.sublogger.info(f'Triggered by {trigger}')
 
         proc = await asyncio.create_subprocess_shell(
             task.command,
@@ -156,17 +182,20 @@ class TaskRunner:
             stderr=task.out_buffer,
         )
 
-        await proc.communicate()
+        exit_code = await proc.wait()
 
-        for new_task in await task.post_process():
-            await self.run_task(new_task)
+        if exit_code == 0:
+            await task.post_process(self)
+            ms = round(1000 * (timer() - start))
+            task.sublogger.info(f'Finished in {ms}ms')
+        else:
+            await task.on_failure(self)
+            ms = round(1000 * (timer() - start))
+            task.sublogger.error(f'Failed in {ms}ms with exit code {exit_code}')
 
         self.active_tasks.remove(task)
 
-        ms = round(1000 * (timer() - start))
-        task.sublogger.info(f'Finished in {ms}ms.')
-
-    async def run_task_debounced(self, task: Task):
+    async def run_task_debounced(self, task: Task, trigger: Optional[str] = None):
         # This means that the task has already been scheduled
         if task in self.last_run_attempt:
             self.last_run_attempt[task] = datetime.now()
@@ -178,54 +207,56 @@ class TaskRunner:
             await asyncio.sleep(DEBOUNCE_THRESHOLD.seconds)
 
         del self.last_run_attempt[task]
-        await self.run_task(task)
+        await self.run_task(task, trigger)
 
-    def schedule(self, task: Task):
-        asyncio.create_task(self.run_task_debounced(task))
+    def schedule(self, task: Task, trigger: Optional[str] = None):
+        asyncio.create_task(self.run_task_debounced(task, trigger))
 
 
 async def iter_file_changes():
-    root = pathlib.Path('.')
-    sublogger = logger.bind(name='<system>')
-
     with Inotify() as inotify:
-        inotify.add_watch(root, Mask.MODIFY)
-        inotify.add_watch(root / 'src', Mask.MODIFY)
-        inotify.add_watch(root / 'figures', Mask.MODIFY)
-        inotify.add_watch(root / 'packages', Mask.MODIFY)
-        sublogger.info('Started daemon and initialized watchers')
+        inotify.add_watch(ROOT_DIR, Mask.MODIFY)
+        inotify.add_watch(ROOT_DIR / 'src', Mask.MODIFY)
+        inotify.add_watch(ROOT_DIR / 'output', Mask.MODIFY)
+        inotify.add_watch(ROOT_DIR / 'figures', Mask.MODIFY)
+        inotify.add_watch(ROOT_DIR / 'packages', Mask.MODIFY)
+        logger.info('Started daemon and initialized watchers')
 
         async for event in inotify:
             if event.path is not None:
-                yield event.path
+                yield pathlib.Path(event.path)
 
 
 async def setup_watchers():
     runner = TaskRunner()
 
     async for path in iter_file_changes():
-        if path == 'tikzcd.cls':
-            runner.schedule(MakeTask(path, 'figures'))
+        if fnmatch(path, 'tikzcd.cls') or fnmatch(path, 'packages/*.sty'):
+            figures_dir = pathlib.Path('figures')
+
+            for figure_path in figures_dir.glob('*.tex'):
+                runner.schedule(TeXTask(figure_path.resolve()), trigger=str(path))
+
+            for figure_path in figures_dir.glob('*.asy'):
+                runner.schedule(AsymptoteTask(figure_path.resolve()), trigger=str(path))
 
         if fnmatch(path, 'figures/*[!_].tex'):
-            runner.schedule(TeXTask(path, path))
+            runner.schedule(TeXTask(path), trigger=str(path))
 
         if fnmatch(path, 'figures/*.asy'):
-            runner.schedule(MakeTask(path, os.path.splitext(path)[0] + '.pdf'))
+            runner.schedule(AsymptoteTask(path), trigger=str(path))
 
-        if fnmatch(path, 'figures/*.pdf'):
-            runner.schedule(TeXTask(path, 'notebook.tex'))
-
-        if fnmatch(path, 'packages/*.sty'):
-            runner.schedule(MakeTask(path, 'figures'))
-            runner.schedule(TeXTask(path, 'notebook.tex'))
-
-        if fnmatch(path, 'src/*.tex') or path == 'notebook.cls':
-            runner.schedule(TeXTask(path, 'notebook.tex'))
+        if fnmatch(path, 'notebook.cls') or fnmatch(path, 'src/*.tex') or fnmatch(path, 'output/*.pdf') or fnmatch(path, 'packages/*.sty'):
+            runner.schedule(TeXTask(pathlib.Path('notebook.tex')), trigger=str(path))
 
 
 if __name__ == '__main__':
     import sys
     logger.remove()
     logger.add(sys.stdout, colorize=True, format='<green>{time:HH:mm:ss}</green> | <level>{level:7}</level> | {extra[name]} | <level>{message}</level>')
-    asyncio.run(setup_watchers())
+
+    with logger.contextualize(name='<system>'):
+        try:
+            asyncio.run(setup_watchers())
+        except KeyboardInterupt:
+            base_logger.info('Gracefully shutting down')
