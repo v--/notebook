43dd58ebb061f27824225a876398b7e4ff9d1b78
Ianis Vasilev <ianis@ivasilev.net>
2024-05-01 01:11:47 +0300

    Improve some unit tests

--- code/notebook/math/natural_deduction/test_proof_tree.py
+++ code/notebook/math/natural_deduction/test_proof_tree.py
@@ -4,17 +4,21 @@ from ..fol.parsing import parse_propositional_formula
 from .axiomatic_derivation import AxiomaticDerivation, derivation_to_proof_tree
 from .minimal_implicational_logic import IMPLICATIONAL_AXIOMS, get_identity_derivation_payload
 from .minimal_logic import minimal_nd_system
-from .proof_tree import AssumptionTree, RuleApplicationTree
+from .proof_tree import AssumptionTree, MarkedFormula, RuleApplicationTree
 from .schemas import FormulaPlaceholder
 from .substitution import UniformSubstitution
 
 
-def test_assumption_tree_to_str() -> None:
+def test_assumption_tree() -> None:
     tree = AssumptionTree(
         minimal_nd_system,
         parse_propositional_formula('p'),
         marker='u'
     )
+
+    assert set(tree.iter_open_assumptions()) == {
+        MarkedFormula(parse_propositional_formula('p'), 'u')
+    }
 
     assert str(tree) == dedent('''\
         [p]ᵘ
@@ -22,7 +26,7 @@ def test_assumption_tree_to_str() -> None:
     )
 
 
-def test_simple_rule_application_tree_to_str() -> None:
+def test_simple_rule_application_tree() -> None:
     substitution = UniformSubstitution({
         FormulaPlaceholder('φ'): parse_propositional_formula('p'),
         FormulaPlaceholder('ψ'): parse_propositional_formula('q')
@@ -53,6 +57,10 @@ def test_simple_rule_application_tree_to_str() -> None:
         ]
     )
 
+    assert set(tree.iter_open_assumptions()) == {
+        MarkedFormula(parse_propositional_formula('(p → q)'), 'u')
+    }
+
     assert str(tree) == dedent('''\
           [(p → q)]ᵘ    [p]ᵛ
           __________________ →⁻
@@ -63,7 +71,7 @@ def test_simple_rule_application_tree_to_str() -> None:
     )
 
 
-def test_complex_rule_application_tree_to_str() -> None:
+def test_complex_rule_application_tree() -> None:
     derivation = AxiomaticDerivation(
         axiom_schemas=IMPLICATIONAL_AXIOMS,
         payload=get_identity_derivation_payload(parse_propositional_formula('p'))
@@ -71,6 +79,7 @@ def test_complex_rule_application_tree_to_str() -> None:
 
     tree = derivation_to_proof_tree(derivation)
 
+    assert set(tree.iter_open_assumptions()) == set()
     assert str(tree) == dedent('''\
         _________________________________________________ Ax    ___________________ Ax
         ((p → ((p → p) → p)) → ((p → (p → p)) → (p → p)))       (p → ((p → p) → p))
