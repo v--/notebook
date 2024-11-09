603c7541f9e414e41c5ecd16d6510602c8611ef0
Ianis Vasilev <ianis@ivasilev.net>
2024-11-09 19:32:33 +0200

    Use a user agent string when fetching from mathnet.ru

--- code/commands/bibtools/sources/mathnet/fetch.py
+++ code/commands/bibtools/sources/mathnet/fetch.py
@@ -1,5 +1,5 @@
 from urllib.error import HTTPError
-from urllib.request import urlopen
+from urllib.request import Request, urlopen
 
 from ...exceptions import BibToolsNetworkError, BibToolsNotFoundError
 from .fixtures import get_mathnet_fixture_path
@@ -19,15 +19,14 @@ FIELDS = [
 
 def fetch_mathnet_html(identifier: str, *, dump_as_fixture: bool = False) -> str:
     try:
-        res = urlopen(
-            f'https://mi.mathnet.ru/{identifier}'
-        )
+        req = Request(f'https://www.mathnet.ru/rus/{identifier}')
+        req.add_header('User-Agent', 'Mozilla/5.0 (X11; Linux x86_64; rv:132.0) Gecko/20100101 Firefox/132.0')
+        res = urlopen(req)
     except HTTPError as err:
         if err.code == 404:
             raise BibToolsNotFoundError(f'Could not find paper with identifier {identifier!r}') from err
 
         raise BibToolsNetworkError('Error while fetching data') from err
-
 
     html = res.read().decode('cp1251')
 
