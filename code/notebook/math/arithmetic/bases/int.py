ccb6005262e07cb4f1993a3f31fb2f9f96179b2c
Ianis Vasilev <ianis@ivasilev.net>
2024-12-16 20:17:34 +0200

    Digit-based arithmetic

--- code/notebook/math/arithmetic/bases/int.py
+++ code/notebook/math/arithmetic/bases/int.py
@@ -1,58 +1,167 @@
-import itertools
 from dataclasses import dataclass
+import itertools
 
+from typing import NamedTuple
 from ...polynomials import monomial
+from ...polynomials.division import DivMod
 from ...polynomials.polynomial.int import IntPolynomial, const, x
 from ..divisibility import int_divmod
-from ..support import sgn
+from ..support import SignT, sgn
 
 
-@dataclass(frozen=True)
+@dataclass
 class IntRadixExpansion:
     radix: int
     polynomial: IntPolynomial
+    sign: SignT
+
+    def __getitem__(self, k: int) -> int:
+        return self.sign * self.polynomial[monomial.x ** k]
+
+    def __setitem__(self, k: int, value: int) -> None:
+        assert 0 <= value < self.radix
+        self.polynomial[monomial.x ** k] = value
 
     def __int__(self) -> int:
-        return self.polynomial(x=self.radix)
+        return self.sign * self.polynomial(x=self.radix)
+
+    def __neg__(self) -> 'IntRadixExpansion':
+        return IntRadixExpansion(self.radix, self.polynomial, -self.sign)
+
+    def __abs__(self) -> 'IntRadixExpansion':
+        return IntRadixExpansion(self.radix, self.polynomial, sign=1)
+
+    @property
+    def max_power(self) -> int:
+        return self.polynomial.total_degree or 0
+
+    def shifted_power(self, k: int) -> 'IntRadixExpansion':
+        pol = IntPolynomial.new_zero()
+
+        for mon in self.polynomial.get_monomials():
+            pol[mon * monomial.x ** k] = self.polynomial[mon]
+
+        return IntRadixExpansion(self.radix, pol, self.sign)
+
+    def with_sign(self, sign: SignT) -> 'IntRadixExpansion':
+        return IntRadixExpansion(self.radix, self.polynomial, sign)
 
     def __str__(self) -> str:
         return str(int(self))
 
 
-# This is alg:nonnegative_integer_radix_expansion in the monograph
+# This is alg:integer_radix_expansion in the monograph
 def get_integer_expansion(n: int, radix: int) -> IntRadixExpansion:
     assert radix >= 2
-    assert n >= 0
 
-    if n < radix:
-        return IntRadixExpansion(radix, n * const)
+    if abs(n) < radix:
+        return IntRadixExpansion(radix, abs(n) * const, sgn(n))
 
-    q, r = int_divmod(n, radix)
+    q, r = int_divmod(abs(n), radix)
     pol = get_integer_expansion(q, radix).polynomial
-    return IntRadixExpansion(radix, pol * x + r * const)
+    return IntRadixExpansion(radix, pol * x + r * const, sgn(n))
 
 
 # This is alg:addition_with_carrying in the monograph
-def add_with_carrying(n: int, m: int, radix: int) -> int:
-    if n < 0:
-        return -add_with_carrying(-n, -m, radix)
+def add_with_carrying(n: IntRadixExpansion, m: IntRadixExpansion) -> IntRadixExpansion:
+    assert n.radix == m.radix
 
-    if m < 0 and -m > n:
-        return -add_with_carrying(-m, -n, radix)
+    if n.sign == -1:
+        return -add_with_carrying(-n, -m)
 
-    pol_n = get_integer_expansion(n, radix).polynomial
-    pol_m = get_integer_expansion(abs(m), radix).polynomial
-    pol = 0 * const
+    if m.sign == -1 and int(-m) > int(n):
+        return -add_with_carrying(-m, -n)
 
+    radix = n.radix
+    exp = IntRadixExpansion(radix, IntPolynomial.new_zero(), sign=1)
     q_k = 0
-    max_power = max(pol_n.total_degree or 0, pol_m.total_degree or 0) + 1
+    max_power = max(n.max_power, m.max_power) + 1
 
     for k in range(max_power + 1):
-        mon = monomial.x ** k
-        a_k = pol_n[mon]
-        c_k = pol_m[mon]
+        sum_ = n[k] + m[k] + q_k
+        # This should match q_k, d_k = int_divmod(sum_, radix)
+        q_k = 1 if sum_ >= radix else -1 if sum_ < 0 else 0
+        d_k = sum_ - q_k * radix
+        exp[k] = d_k
 
-        q_k, d_k = int_divmod(a_k + sgn(m) * c_k + q_k, radix)
-        pol[mon] = d_k
+    return exp
 
-    return pol(x=radix)
+
+# This is alg:single_digit_multiplication_with_carrying in the monograph
+def single_digit_mult_with_carrying(n: IntRadixExpansion, m: int) -> IntRadixExpansion:
+    assert abs(m) < n.radix
+
+    if n.sign == -1 and m < 0:
+        return single_digit_mult_with_carrying(-n, -m)
+
+    if n.sign * sgn(m) == -1:
+        return -single_digit_mult_with_carrying(abs(n), abs(m))
+
+    radix = n.radix
+    exp = IntRadixExpansion(radix, IntPolynomial.new_zero(), sign=1)
+
+    q_k = 0
+    max_power = n.max_power + 1
+
+    for k in range(max_power + 1):
+        prod = m * n[k] + q_k
+        prod_exp = get_integer_expansion(prod, radix)
+        q_k = prod_exp[1]
+        d_k = prod_exp[0]
+
+        exp[k] = d_k
+
+    return exp
+
+
+# This is alg:multi_digit_multiplication_with_carrying in the monograph
+def multi_digit_mult_with_carrying(n: IntRadixExpansion, m: IntRadixExpansion) -> IntRadixExpansion:
+    assert n.radix == m.radix
+
+    radix = n.radix
+    exp = IntRadixExpansion(radix, IntPolynomial.new_zero(), sign=1)
+    max_power = n.max_power + m.max_power
+
+    for k in range(max_power + 1):
+        res = single_digit_mult_with_carrying(n, m[k])
+        exp = add_with_carrying(exp, res.shifted_power(k))
+
+    return exp
+
+
+class ExpansionDivMod(NamedTuple):
+    quot: IntRadixExpansion
+    rem: IntRadixExpansion
+
+
+# This is alg:long_integer_division in the monograph
+def long_division(n: IntRadixExpansion, m: IntRadixExpansion) -> ExpansionDivMod:
+    assert n.radix == m.radix
+    assert m.sign != 0
+    radix = n.radix
+
+    if n.sign == -1:
+        quot, rem = long_division(-n, abs(m))
+        quot_ = add_with_carrying(quot, get_integer_expansion(1, radix)).with_sign(-m.sign)
+        rem_ = add_with_carrying(abs(m), -rem)
+        return ExpansionDivMod(quot_, rem_)
+
+    if m.sign == -1:
+        quot, rem = long_division(n, -m)
+        return ExpansionDivMod(-quot, rem)
+
+    rem = n
+    quot = IntRadixExpansion(radix, IntPolynomial.new_zero(), sign=1)
+
+    for s in range(n.max_power - m.max_power + 1):
+        k = n.max_power - m.max_power - s
+        m_ = m.shifted_power(k)
+        q_k = max(q for q in range(radix) if q * int(m_) <= int(rem))
+        quot[k] = q_k
+
+        rem = add_with_carrying(
+            rem,
+            -single_digit_mult_with_carrying(m_, q_k)
+        )
+
+    return ExpansionDivMod(quot, rem)
--- code/notebook/math/arithmetic/bases/test_int.py
+++ code/notebook/math/arithmetic/bases/test_int.py
@@ -1,20 +1,30 @@
 from ....support.pytest import pytest_parametrize_kwargs
 from ...polynomials.polynomial.int import IntPolynomial, const, x, zero
-from .int import add_with_carrying, get_integer_expansion
+from ..support import sgn
+from ..divisibility import quot, rem
+from .int import (
+    IntRadixExpansion,
+    add_with_carrying,
+    get_integer_expansion,
+    long_division,
+    multi_digit_mult_with_carrying,
+    single_digit_mult_with_carrying,
+)
 
 
 @pytest_parametrize_kwargs(
-    dict(n=0, radix=2, pol=zero),
-    dict(n=1, radix=2, pol=const),
-    dict(n=2, radix=2, pol=x),
-    dict(n=3, radix=2, pol=x + const),
-    dict(n=4, radix=2, pol=x ** 2),
-    dict(n=5, radix=2, pol=x ** 2 + const),
-    dict(n=8, radix=2, pol=x ** 3),
-    dict(n=8, radix=3, pol=2 * (x + const)),
+    dict(n=0,  radix=2, pol=zero),
+    dict(n=1,  radix=2, pol=const),
+    dict(n=2,  radix=2, pol=x),
+    dict(n=3,  radix=2, pol=x + const),
+    dict(n=4,  radix=2, pol=x ** 2),
+    dict(n=5,  radix=2, pol=x ** 2 + const),
+    dict(n=8,  radix=2, pol=x ** 3),
+    dict(n=-8, radix=2, pol=x ** 3),
+    dict(n=8,  radix=3, pol=2 * (x + const)),
 )
 def test_integer_expansion(n: int, radix: int, pol: IntPolynomial) -> None:
-    assert get_integer_expansion(n, radix).polynomial == pol
+    assert get_integer_expansion(n, radix) == IntRadixExpansion(radix, pol, sgn(n))
 
 
 @pytest_parametrize_kwargs(
@@ -26,10 +36,60 @@ def test_integer_expansion(n: int, radix: int, pol: IntPolynomial) -> None:
     dict(n=-3, m=5, radix=10),
     dict(n=-3, m=-5, radix=10),
     dict(n=1, m=-9, radix=10),
-    # dict(n=343, m=50315, radix=3),
-    # dict(n=343, m=-50315, radix=3),
-    # dict(n=-343, m=50315, radix=3),
-    # dict(n=-343, m=-50315, radix=3),
+    dict(n=343, m=50315, radix=3),
+    dict(n=343, m=-50315, radix=3),
+    dict(n=-343, m=50315, radix=3),
+    dict(n=-343, m=-50315, radix=3),
 )
 def test_add_with_carrying(n: int, m: int, radix: int) -> None:
-    assert add_with_carrying(n, m, radix) == n + m
+    n_ = get_integer_expansion(n, radix)
+    m_ = get_integer_expansion(m, radix)
+    result = add_with_carrying(n_, m_)
+
+    assert int(result) == n + m
+
+
+@pytest_parametrize_kwargs(
+    dict(n=0, m=0, radix=10),
+    dict(n=3, m=0, radix=2),
+    dict(n=3, m=5, radix=10),
+    dict(n=3, m=-5, radix=10),
+    dict(n=-3, m=5, radix=10),
+    dict(n=-3, m=-5, radix=10),
+    dict(n=1, m=-9, radix=10)
+)
+def test_single_digit_mult_with_carrying(n: int, m: int, radix: int) -> None:
+    n_ = get_integer_expansion(n, radix)
+    result = single_digit_mult_with_carrying(n_, m)
+    assert int(result) == n * m
+
+
+@pytest_parametrize_kwargs(
+    dict(n=343, m=50315, radix=3),
+    dict(n=343, m=-50315, radix=3),
+    dict(n=-343, m=50315, radix=3),
+    dict(n=-343, m=-50315, radix=3),
+)
+def test_multi_digit_mult_with_carrying(n: int, m: int, radix: int) -> None:
+    n_ = get_integer_expansion(n, radix)
+    m_ = get_integer_expansion(m, radix)
+    result = multi_digit_mult_with_carrying(n_, m_)
+    assert int(result) == n * m
+
+
+@pytest_parametrize_kwargs(
+    dict(n=1, m=2, radix=10),
+    dict(n=2, m=2, radix=10),
+    dict(n=19, m=3, radix=10),
+    dict(n=21, m=3, radix=10),
+    dict(n=50315,  m=343,  radix=10),
+    dict(n=-50315, m=343,  radix=10),
+    dict(n=50315,  m=-343, radix=10),
+    dict(n=-50315, m=-343, radix=10),
+)
+def test_long_division(n: int, m: int, radix: int) -> None:
+    n_ = get_integer_expansion(n, radix)
+    m_ = get_integer_expansion(m, radix)
+    q, r = long_division(n_, m_)
+    assert int(q) == quot(n, m)
+    assert int(r) == rem(n, m)
