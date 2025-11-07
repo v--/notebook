115e463a89cb0e2d84b144442ef6836fda131c39
Ianis Vasilev <ianis@ivasilev.net>
2025-11-07 17:39:22 +0200

    Refine the logistic map bifurcation diagram

--- src/notebook/figures/ex__def__logistic_map__orbits.py
+++ src/notebook/figures/ex__def__logistic_map__orbits.py
@@ -1,7 +1,7 @@
 import math
-import os
 import pathlib
 import shutil
+import subprocess
 import tempfile
 from concurrent.futures import ThreadPoolExecutor
 from textwrap import dedent
@@ -18,40 +18,44 @@ raster_path = tmp_path / 'intermediate.eps'  # Asymptote requires EPS, which doe
 code_path = tmp_path / 'diagram.asy'
 
 
-x_resolution = 3000
+x_resolution = 1500
 x_0 = 0.5
 
-a_resolution = 4500
+a_resolution = 3000
 a_min = 2
 a_max = 4
 
-iterations = 1000
-min_iterations = 100
+max_iterations = 1750
+min_iterations = 1000
+
+max_threads = 20
+pixels_per_thread = a_resolution // max_threads
+pixels = np.full((x_resolution, a_resolution), dtype=np.uint8, fill_value=np.iinfo(np.uint8).max)
 
 
-def fill_pixels(j: int) -> np.ndarray:
-    a = a_min + (a_max - a_min) * j / a_resolution
+def fill_column(j: int) -> None:
+    a = a_min + ((a_max - a_min) * j / a_resolution)
     x = x_0
-    column = np.full(x_resolution, dtype=np.uint8, fill_value=255)
 
-    for it in range(iterations):
+    for it in range(max_iterations):
         x = a * x * (1 - x)
 
         if it >= min_iterations:
             i = x_resolution - math.floor(x * x_resolution)
 
             if 0 <= i < x_resolution:
-                column[i] = 0
+                pixels[i, j] = 0
 
-    return column
+
+def fill_columns(thread_index: int) -> None:
+    for j in range(thread_index * pixels_per_thread, (thread_index + 1) * pixels_per_thread):
+        fill_column(j)
 
 
 with ThreadPoolExecutor() as executor:
-    columns = list(executor.map(fill_pixels, list(range(a_resolution))))
+    list(executor.map(fill_columns, list(range(max_threads))))
 
 
-# We concatenate along axis 1 and not 0 because Pillow expects a transposed matrix
-pixels = np.stack(columns, axis=1)
 img = PIL.Image.fromarray(pixels, 'L')
 img.save(raster_path)
 
@@ -70,12 +74,15 @@ with open(code_path, 'w') as code_file:
 
             xaxis(
               Label('$a$'),
+              p=fontsize(7),
               axis=Bottom,
               ticks=Ticks(pTick=pens.thin, Step=0.5, step=0.1)
             );
 
             yaxis(
-              Label('$x$'),
+              Label('$\\Phi_{{a,t}}(x_0)$'),
+              p=fontsize(7),
+              autorotate=false,
               axis=Left,
               ticks=Ticks(pTick=pens.thin, Step=0.2, step=0.1)
             );
@@ -83,5 +90,9 @@ with open(code_path, 'w') as code_file:
         )
     )
 
-os.system(f'asy -dir={ROOT_PATH} -outname={output_path} {code_path}')  # noqa: S605
-shutil.rmtree(tmp_path)
+try:
+    subprocess.run(f'asy -dir={ROOT_PATH} -outname={output_path} {code_path}', shell=True, check=True)  # noqa: S602
+except subprocess.CalledProcessError as err:
+    raise SystemExit(1) from err
+finally:
+    shutil.rmtree(tmp_path)
