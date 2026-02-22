c697224cf2ae9876ce669a5606ab923fa00d9c06
Ianis Vasilev <ianis@ivasilev.net>
2026-02-22 16:25:53 +0200

    Fix unit tests

--- src/notebook/math/logic/cnf_and_dnf/test_formula_to_cnf_or_dnf.py
+++ src/notebook/math/logic/cnf_and_dnf/test_formula_to_cnf_or_dnf.py
@@ -1,5 +1,5 @@
-from .....support.pytest import pytest_parametrize_kwargs
-from ...propositional import are_semantically_equivalent, parse_prop_formula
+from ....support.pytest import pytest_parametrize_kwargs
+from ..propositional import are_semantically_equivalent, parse_prop_formula
 from .formula_to_cnf_or_dnf import formula_to_cnf_prop
 from .validation import is_formula_in_cnf
 
