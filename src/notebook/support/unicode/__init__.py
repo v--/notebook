abab54e4fab960caf932214a5188582256f25422
Ianis Vasilev <ianis@ivasilev.net>
2026-06-15 00:37:00 +0300

    Expose unicode errors

--- src/notebook/support/unicode/__init__.py
+++ src/notebook/support/unicode/__init__.py
@@ -1,3 +1,4 @@
 from .alphabets import Capitalization, is_greek_string, is_latin_string
+from .exceptions import NotebookUnicodeError, UnrecognizedCharacterError
 from .scripts import atoi_subscripts, is_numeric_subscript_char, itoa_subscripts, itoa_superscripts, to_superscript
 from .whitespace import normalize_whitespace
