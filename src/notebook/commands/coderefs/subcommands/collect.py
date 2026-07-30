9214a6cbe05f01f16f52ec7de0377f745685b96e
Ianis Vasilev <ianis@ivasilev.net>
2026-07-31 00:57:30 +0300

    Fix collector

--- src/notebook/commands/coderefs/subcommands/collect.py
+++ src/notebook/commands/coderefs/subcommands/collect.py
@@ -1,4 +1,5 @@
 import importlib
+import importlib.machinery
 import pathlib
 import pkgutil
 
@@ -13,10 +14,16 @@ def recursively_load_modules(module_path: pathlib.Path, module_name: str) -> Non
     importlib.import_module(module_name)
 
     for submodule_info in pkgutil.walk_packages([module_path]):
-        recursively_load_modules(
-            module_path / submodule_info.name,
-            module_name + '.' + submodule_info.name,
-        )
+        if (
+            isinstance(submodule_info.module_finder, importlib.machinery.FileFinder) and
+            # I started getting mismatches here; It may be a Python bug.
+            # TODO: Revise
+            pathlib.Path(submodule_info.module_finder.path).is_relative_to(module_path)
+        ):
+            recursively_load_modules(
+                module_path / submodule_info.name,
+                module_name + '.' + submodule_info.name,
+            )
 
 
 @coderefs.command()
